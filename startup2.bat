6fc035f294c0221afa464927656aa51520344270
Alan Akins <alan.akins@gmail.com>
2020-09-14 10:01:10 -0500

    Remove old files to clean up structure.

--- startup2.bat
+++
@@ -1,4 +0,0 @@
-call C:\Users\aakins\AppData\Local\Continuum\anaconda3\Scripts\activate new
-call C:\Users\aakins\Envs\Analysis2\Scripts\activate.bat
-call python C:\Projects\OAanalysis\waitress_server.py
-pause
